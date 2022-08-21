e8898ea32ac7558a9fc1c7ce2aea992b899d9645
marciovalim <marciogsvalim@gmail.com>
2022-08-20 22:35:54 -0300

    👤 added users routes

--- src/index.js
+++ src/index.js
@@ -5,4 +5,28 @@ const app = express();
 app.use(express.json());
 app.listen(8080, () => console.log("🚀 Server running..."));
 
+const users = [];
+
+function findUserByUsername(username) {
+  return users.find((user) => user.username == username);
+}
+
+app.post("/users", (req, res) => {
+  const { name, username } = req.body;
+
+  if (findUserByUsername(username)) {
+    res.status(400).json({ error: "Username already exists" });
+  }
+
+  const user = {
+    id: v4(),
+    name: name,
+    username,
+    todos: [],
+  };
+  users.push(user);
+
+  return res.status(201).json(user);
+});
+
 module.exports = app;
